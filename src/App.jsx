99adf0e98b930959f12fc16470f8da4aa4fbc3f2
Dalip Tandon <daliptandon8@gmail.com>
2025-01-22 22:17:34 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";  // Import Router
 import About from "./About";
 import Hero from "./Hero";
 import Intro from "./Intro";
+import Navbar from "./Navbar";
+
 function App() {
-
   return (
-    <div className="h-screen">
-      
-
-     <Hero/>
-     <About/>
-     <Intro/>
-    </div>
-  )
+    <Router> {/* Wrap your components inside BrowserRouter */}
+      <div className="h-screen">
+        <Navbar />  {/* Assuming you have links inside Navbar for routing */}
+        <Hero />
+        <About />
+        <Intro />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
